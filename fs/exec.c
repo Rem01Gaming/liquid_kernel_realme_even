d2d96d162e02aad1cc13b62533f6c16960b51318
TheMalachite <eliasgheeraert@gmail.com>
2023-09-18 21:17:53 +0530

    treewide: Drop Oplus security changes
    - Oplus security changes completely nuke support SELinux permissive, block system mounting and spam logs for nothing
    
    Change-Id: I73263b05cf2102aeef753a00f576e607972c5bf1

--- fs/exec.c
+++ fs/exec.c
@@ -1693,12 +1693,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-#ifdef CONFIG_OPLUS_SECURE_GUARD
-#if defined(CONFIG_OPLUS_EXECVE_BLOCK) || defined(CONFIG_OPLUS_EXECVE_REPORT)
-extern int oplus_exec_block(struct file *file);
-#endif /* CONFIG_OPLUS_EXECVE_BLOCK or CONFIG_OPLUS_EXECVE_REPORT */
-#endif /* CONFIG_OPLUS_SECURE_GUARD */
-
 #ifdef CONFIG_KSU
 extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
@@ -1767,16 +1761,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	retval = PTR_ERR(file);
 	if (IS_ERR(file))
 		goto out_unmark;
-
-#ifdef CONFIG_OPLUS_SECURE_GUARD
-#if defined(CONFIG_OPLUS_EXECVE_BLOCK) || defined(CONFIG_OPLUS_EXECVE_REPORT)
-    retval = oplus_exec_block(file);
-	if (retval){
-		fput(file);
-		goto out_unmark;
-	}
-#endif /* CONFIG_OPLUS_EXECVE_BLOCK or CONFIG_OPLUS_EXECVE_REPORT */
-#endif /* CONFIG_OPLUS_SECURE_GUARD */
+	
 	sched_exec();
 
 	bprm->file = file;
